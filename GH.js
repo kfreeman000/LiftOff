295d2b2c12b962c593e163d920aab581f74acdf8
matthewstevens <mstevens@ithaca.edu>
2024-10-28 20:55:26 -0400

    Preliminary app layout

---
+++ GH.js
@@ -0,0 +1,91 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Lift Off</Text>
+      <Text>{'\n'}</Text>
+      <Image source={require('./images/unnamed.png')} style={{width: 200,     height: 200}}/>
+      <Text>{'\n\n'}</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('Reps')} activeOpacity={0.2}>
+         <Text style={styles.button}>Rep Calculator</Text>
+         </TouchableOpacity>
+         <Text>{'\n\n'}</Text>
+         <TouchableOpacity onPress={() => navigation.navigate('Reps')} activeOpacity={0.2}>
+         <Text style={styles.button}>Submit Workout</Text>
+         </TouchableOpacity>
+         <Text>{'\n\n'}</Text>
+         <TouchableOpacity onPress={() => navigation.navigate('Reps')} activeOpacity={0.2}>
+         <Text style={styles.button}>Achievements</Text>
+         </TouchableOpacity>
+    </View>
+  );
+}
+
+function RepScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Rep Calculator</Text>
+      <Text>{'\n'}</Text>
+      <Image source={require('./images/unnamed.png')} style={{width: 200,     height: 200}}/>
+    </View>
+  );
+}
+
+function SubmitScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Submit Workout Screen</Text>
+      <Text>{'\n'}</Text>
+      <Image source={require('./images/unnamed.png')} style={{width: 200,     height: 200}}/>
+    </View>
+  );
+}
+
+function AchievementsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Achievements</Text>
+      <Text>{'\n'}</Text>
+      <Image source={require('./images/unnamed.png')} style={{width: 200,     height: 200}}/>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Reps" component={RepScreen} />
+        <Stack.Screen name="Submit" component={SubmitScreen} />
+        <Stack.Screen name="Achievements" component={AchievementsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: "center",
+    marginTop:10,
+    paddingTop:15,
+    paddingBottom:15,
+    marginLeft:30,
+    marginRight:30,
+    borderRadius:10,
+    borderWidth: 1,
+    backgroundColor: "#40E0D0",
+    padding: 10,
+     overflow: "hidden"
+  }
+}
+)
+
+export default App;
