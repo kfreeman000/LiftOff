8cc083336179efcece64aaa6f7389fcdd0e6437d
matthewstevens <mstevens@ithaca.edu>
2024-11-12 10:10:02 -0500

    Stuff from last part

--- GH.js
+++ GH.js
@@ -2,28 +2,29 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import WorkoutForm from './workout.JS';
 
-function HomeScreen({navigation}) {
+function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Lift Off</Text>
       <Text>{'\n'}</Text>
-      <Image source={require('./images/unnamed.png')} style={{width: 200,     height: 200}}/>
+      <Image source={require('./images/unnamed.png')} style={{ width: 200, height: 200 }} />
       <Text>{'\n\n'}</Text>
       <TouchableOpacity onPress={() => navigation.navigate('Reps')} activeOpacity={0.2}>
-         <Text style={styles.button}>Rep Calculator</Text>
-         </TouchableOpacity>
-         <Text>{'\n\n'}</Text>
-         <TouchableOpacity onPress={() => navigation.navigate('Reps')} activeOpacity={0.2}>
-         <Text style={styles.button}>Submit Workout</Text>
-         </TouchableOpacity>
-         <Text>{'\n\n'}</Text>
-         <TouchableOpacity onPress={() => navigation.navigate('Reps')} activeOpacity={0.2}>
-         <Text style={styles.button}>Achievements</Text>
-         </TouchableOpacity>
+        <Text style={styles.button}>Rep Calculator</Text>
+      </TouchableOpacity>
+      <Text>{'\n\n'}</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('Submit')} activeOpacity={0.2}>
+        <Text style={styles.button}>Submit Workout</Text>
+      </TouchableOpacity>
+      <Text>{'\n\n'}</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('Achievements')} activeOpacity={0.2}>
+        <Text style={styles.button}>Achievements</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 function RepScreen() {
   return (
@@ -35,13 +36,9 @@ function RepScreen() {
   );
 }
 
-function SubmitScreen() {
+function WorkoutScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Submit Workout Screen</Text>
-      <Text>{'\n'}</Text>
-      <Image source={require('./images/unnamed.png')} style={{width: 200,     height: 200}}/>
-    </View>
+      <WorkoutForm />
   );
 }
 
@@ -63,7 +60,7 @@ function App() {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Reps" component={RepScreen} />
-        <Stack.Screen name="Submit" component={SubmitScreen} />
+        <Stack.Screen name="Submit" component={WorkoutScreen} />
         <Stack.Screen name="Achievements" component={AchievementsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
