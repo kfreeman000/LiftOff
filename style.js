4fa4c27463f06dd9943257ae79805261d80a6d02
cprochilo <cprochilo@ithaca.edu>
2024-12-03 13:28:00 -0500

    Updated style.js

--- style.js
+++ style.js
@@ -63,6 +63,12 @@ const styles = StyleSheet.create({
       borderRadius: 25,
       backgroundColor: "#60B5F9",
       overflow: "hidden"
+    },
+    blueHeaderText: {
+      fontFamily: 'Comfortaa-Bold',
+      color: "#60B5F9",
+      fontSize: 18,
+      textAlign: 'center'
     }
   }
   )
