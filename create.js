c4f1448b1bffc2f478ed43c9ea47bf8eaf8200ca
Katherine Freeman <kfreeman@ithaca.edu>
2026-01-19 16:00:06 -0500

    email auth

--- create.js
+++ create.js
@@ -19,7 +19,7 @@ import { useNavigation } from '@react-navigation/native';
 
 import { auth, db } from './firebase';
 import { createUserWithEmailAndPassword, sendEmailVerification  } from 'firebase/auth';
-import { doc, setDoc } from 'firebase/firestore';
+import { doc, setDoc, serverTimestamp } from 'firebase/firestore';
 
 export default function CreateAcc() {
   const navigation = useNavigation();
